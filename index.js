bb84a6e72534c9f1953af3b83499a3fd7c90e9cf
WILLIAM PEREIRA DA CONCEICAO <74944280+willtechcod@users.noreply.github.com>
2023-06-04 13:12:08 -0700

    Update index.js
    up

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ app.listen(process.env.PORT || PORT, () => {
 
 const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: false,
+    port: 587,
+    secure: true,
     auth: {
         user: process.env.USER,
         pass: process.env.PASS,
